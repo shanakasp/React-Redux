88a9328c748eefb870b5240a8d6559d48378289d
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-28 20:13:12 +0530

    modified index.js in src

--- src/features/Users.js
+++ src/features/Users.js
@@ -12,3 +12,5 @@ export const userSlice = createSlice(
         }
     }
 )
+
+export default userSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 
+import userReducer from './features/Users';
+
 const store = configureStore({
   reducer:{
-    // users:
-  }
+   users: userReducer,
+  },
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
