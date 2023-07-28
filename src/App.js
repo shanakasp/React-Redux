bb32cb4784f7df894da8558c33ded34cdc5b6c90
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-28 20:38:10 +0530

    modified App.js and Users.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {addUser} from './features/Users';
 
 function App() {
 
-  const userList = useSelector((state) => state.users.value)
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+ const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+
   return (
     <div className="App">
     <div className='adduser'>
-      <input type='text' placeholder='Name...'></input>
-      <input type='text' placeholder='Username...'></input>
-      <button> Add User </button>
+      <input type='text' placeholder='Name...' 
+      onChange={(event) => {setName(event.target.value)}}></input>
+      <input type='text' placeholder='Username...'
+       onChange={(event) => {setUsername(event.target.value)}}></input>
+      <button onClick={(() => {dispatch(addUser({id:0 , name
+      ,username}))})}> {" "} Add User </button>
     </div>
-    <div className='dsiplayUser'></div>
+    <div className='dsiplayUser'>
+      {userList.map((user) =>
+      {
+        return <div><h1> {user.name}</h1>
+        <h1>{user.username}</h1> </div>
+      })}
+    </div>
     </div>
   );
 }
--- src/features/Users.js
+++ src/features/Users.js
@@ -7,10 +7,10 @@ export const userSlice = createSlice(
         initialState: {value: UsersData},
         reducers: {
             addUser: (state, action) => {
-
+                state.value.push(action.payload);
             }
         }
     }
-)
-
+);
+export const {addUser} = userSlice.actions;
 export default userSlice.reducer;
